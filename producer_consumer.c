fffdc0d879b55fc51c14059c85af4a52fc5c7d91
Aaron Huggins <90935816+MaelstromDef@users.noreply.github.com>
2022-10-23 14:50:14 -0700

    Create Producer Function
    Created producer function and did some code organization.

--- producer_consumer.c
+++ producer_consumer.c
@@ -8,6 +8,7 @@
 #include <linux/sched/signal.h>
 #include <linux/semaphore.h>
 
+// SET MODULE PARAMS
 int buffSize;
 int prod;
 int cons;
@@ -16,77 +17,185 @@ module_param(buffSize, int, S_IRUSR | S_IWUSR);
 module_param(prod, int, S_IRUSR | S_IWUSR);
 module_param(cons, int, S_IRUSR | S_IWUSR);
 module_param(uuid, int, S_IRUSR | S_IWUSR);
+
+// CONSTRUCT SEMAPHORES
+struct semaphore empty;	// Size: buffSize. All locks available when buffer is empty
+struct semaphore full;	// Size: buffSize. All locks available when buffer is full
+struct semaphore mutex;	// Size: 1. Lock is available when none are accessing buffer
+sema_init(&empty, buffSize);
+sema_init(&full, buffSize);
+sema_init(&mutex, 1);
+
+// Use up all locks on full
+while (down_trylock(&full));
+
+// PRODUCER FUNCTION
+
+/*
+
+producer takes a list of tasks and a buffer for those tasks,
+and goes through each task in the list in order to add them to 
+the buffer. However, it can only access the buffer so long as it
+has the mutex lock. Additionally, once it has the mutex lock, it checks
+to see if the buffer is available for insertion.
+
+If the buffer is not available for task insertion, the producer
+releases its mutex lock and repeats its attempt for the given task.
+
+If the buffer is available for task insertion, the producer acquires
+a lock for empty 
+				(which has count 0 once there are no empty buffer cells, ie [task, task]),
+inserts the task into the buffer,
+and releases a lock for full 
+				(which has count 0 once there are no used buffer cells, ie [null, null])
+
+Return values:
+	Successfully went through all tasks: 0
+	Failure to go through all tasks: 1
+*/
+int producer(task_struct* tasks, task_struct* buffer) {
+	for_each_process(tasks) {
+		// Continue until thread stop point or item placed in buffer
+		while (!kthread_should_stop()) {
+
+			// Acquire buffer mutex lock (wait until acquired)
+			if (down_interruptible(&mutex)) {
+
+				// Acquire lock from empty if available
+				if (down_trylock(&empty)) {
+					buffer[buffSize - empty.count] = *tasks;
+
+					up(&full);	// Release 1 lock for an item in the buffer
+					up(&mutex);	// Release buffer mutex lock
+					break;
+				}
+
+				up(&mutex);	// Release buffer mutex lock
+			}
+		}
+		
+		if (kthread_should_stop()) return 1;
+	}
+
+	return 0;
+}
+
+/*
+* CODE EXAMPLES BELOW
+*/
+
+// Set module parameters
+int buffSize;
+int prod;
+int cons;
+int uuid;
+module_param(buffSize, int, S_IRUSR | S_IWUSR);
+module_param(prod, int, S_IRUSR | S_IWUSR);
+module_param(cons, int, S_IRUSR | S_IWUSR);
+module_param(uuid, int, S_IRUSR | S_IWUSR);
+
 //int param_var;
 //module_param(param_var,int, S_IRUSR | S_IWUSR);
 
+// Displays module parameter values
 void display(void){
 	printk(KERN_ALERT "buffsize: param = %d", buffSize);
 	printk(KERN_ALERT "prod: param = %d", prod);
 	printk(KERN_ALERT "cons: param = %d", cons);
 	printk(KERN_ALERT "uuid: param = %d", uuid);
-	}
+}
 	
+// Initializer: Says hello and displays 'module_param's
 static int hello_init(void){
-	printk(KERN_ALERT "TEST: hello");
+	// printk(KERN_ALERT "TEST: hello");	// Test initializer
 	display();
 	return 0;
-	}
-	
-static void hello_exit(void){
-    unsigned long long timeRun = 0;
-    unsigned long timeRunS, timeRunM, timeRunH;
+}
 
+// Destructor: Runs a list of tasks and accumulates the time for the list to run
+static void hello_exit(void){
+    unsigned long timeRun = 0;
+    unsigned long timeRunS, timeRunM, timeRunH;
+	
     for_each_process(ts1){					//For all processes in the task list
-/*Still needs semphore creation to functionally destroy the semaphores*/
+		/*Still needs semphore creation to functionally destroy the semaphores*/
         up(ts1);						//Set flag to up
         timeRun += (ktime_get_ns() - ts1->start_time);		//Accumulate run time
 
         kthread_stop(ts1);					//Stop that thread
     }
-    timeRunS = (unsigned long) (timeRun % 100000000);		//ns to s
+    timeRunS = timeRun % 100000000;				//ns to s
     timeRunM = timeRunS % 60;					//s to min
     timeRunH = timeRunM % 60;					//min to hour
     
     timeRunS = timeRunS - (timeRunM*60);			//Find seconds between minutes running
     timeRunM = timeRunM - (timeRunH*60);			//Find minutes between hours running
 
-    printk(KERN_INFO "The total elapsed time of all processes for UID %s is %d : %d : %d\n", uuid, timeRunH, timeRunM, timeRunS);
+    printk(KERN_INFO "The total elapsed time of all processes for UID %s is\t%d:%d:%d\n", uuid, timeRunH, timeRunM, timeRunS);
 }
 	
+/* 
+threadfn is the function to run in the thread; 
+data is the data pointer for threadfn; 
+namefmt is the name for the thread. 
 
-struct task_struct *kthread_run(int (*threadfn)(void *data), void *data, const char *namefmt)
+On Success: Returns pointer to thread's task_struct
+On Failure: Returns ERR_PTR(-ENOMEM)
+*/
+// struct task_struct* kthread_run(int (*threadfn)(void* data), void* data, const char* namefmt);
+
+// Example code to create kernel thread:
 
 static int kthread_func(void *arg){
-
+	// Some code
 }
 
+// Create and run "thread-1"
 ts1 = kthread_run(kthread_func, NULL, "thread-1");
 
 
-module_init(hello_init);
-module_exit(hello_exit);	
+module_init(hello_init);	// Initialize function: hello_init
+module_exit(hello_exit);	// Exit function: hello_exit
 MODULE_LICENSE("GPL");
 	
 	
-struct task_struct* p;
-size_t process_counter = 0;
+struct task_struct* p;			// List of tasks
+size_t process_counter = 0;		// Holds number of tasks
+
+// Count number of processes in task list
 for_each_process(p){
 	++process_counter;
-	}	
-	
-struct semaphore empty;
-static inline vid sema_init(struct semaphore *sem, int val);
-void down_interruptible(struct semaphore *sem);
-void up(struct semaphore *sem);
+}	
 
-sema_init(&empty, 5);
+// Semaphores
+struct semaphore empty;	// Size: bufSize. All locks available when buffer is empty
+struct semaphore full;	// Size: bufSize. All locks available when buffer is full
+struct semaphore mutex;	// Size: 1. Lock is available when none are accessing buffer
+sema_init(&mutex, 1);
+
+/*
+* 
+* List of semaphore functions
+
+static inline vid sema_init(struct semaphore *sem, int val);	// Initializes semaphore using a value val
+void down_interruptible(struct semaphore *sem);					// Locks a semaphore
+void up(struct semaphore *sem);									// Releases a semaphore lock
+*/
+
+sema_init(&empty, 5);	// Initialize 'empty' to 5
 while(!kthread_should_stop())
 {
+	/*
+		Acquires a lock from empty, and waits to acquire if lock is unavailable
+	*/
 	if(down_interruptible(&empty))
 	{
 		break;
 	}
-	up(empty);
+
+	// Some code...
+
+	up(empty);	// Release lock from empty
 }
 	
 	
