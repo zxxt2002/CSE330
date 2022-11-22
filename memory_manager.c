f222d852694aaf6345e96de002517221045212cc
Arjun Dadhwal <37658489+Ardad2@users.noreply.github.com>
2022-11-22 11:56:31 -0700

    Finalizing the code.

--- memory_manager.c
+++ memory_manager.c
@@ -7,150 +7,174 @@
 #include <linux/hrtimer.h>
 
 //The process ID (stored in pid variable) is taken as the command-line input argument.
-static int pid = 0;
-module_param(pid, int, 0);
+static int pid = -1;
+module_param(pid, int, 0); //Pass the input argument to this kernel module
 
-//Declaring variables
 
-struct task_struct *task;
-unsigned long address = 0;
-struct vm_area_struct *vma;
-pte_t *ptep_curr;
+/*Declaring variables to store the total number of pages that are present in the Resident Set Size (RSS), SWAP, Working Set Size (WSS) respectively
+and later used to calculate their size from it.*/
 
-//Declaring variables to store the total number of pages that are present in the Resident Set Size (RSS), SWAP, Working Set Size (WSS) respectively. 
+unsigned long rss_count = 0;
+unsigned long swap_count = 0;
+unsigned long wss_count = 0;
 
-unsigned long RSS = 0;
-unsigned long SWAP = 0;
-unsigned long WSS = 0;
+//Variables that will help with walking through the page tables.
+
+unsigned long page_address = 0; //Used to store the page address.
+pte_t *curr_pte; //The pointer to currently traversed page entry.
 
 //This function walks through each valid process' page tables and checks if the page is present in the physical memory or in the swap. 
 
 static void walk_page_tables(struct mm_struct *mm, unsigned long address){
 
+	//Pointers to help with going through the five levels of page tables.
+	
 	pgd_t *pgd;
 	p4d_t *p4d;
 	pte_t *ptep, pte;
 	pud_t *pud;
 	pmd_t *pmd;
+	
+	//Five levels (PGD, P4D, PTE, PUD, PMD) of page levels are walked through.
 		
 	//Get pgd from mm and the page address
 
-	pgd = pgd_offset(mm,address);
+	pgd = pgd_offset(mm,page_address);
 	if (pgd_none(*pgd) || pgd_bad(*pgd)){
-		return;
+		return; //Check if pgd is bad or does not exist
 	}
 	
 	//Get p4d from pgd and the page address
 	
-	p4d = p4d_offset(pgd,address);
+	p4d = p4d_offset(pgd,page_address);
 	if (p4d_none(*p4d) || p4d_bad(*p4d)) {
-		return;
+		return; //Check if p4d is bad or does not exist
 	}
 	//Get pud from p4d and the page address
 	
-	pud = pud_offset(p4d,address);
+	pud = pud_offset(p4d,page_address);
 	if (pud_none(*pud) || pud_bad(*pud)) {
-		return;
+		return; //Check if pud is bad or does not exist
 	}
 	//Check if pmd is bad or does not exist
 	
-	pmd = pmd_offset(pud, address);
+	pmd = pmd_offset(pud, page_address);
 	if (pmd_none(*pmd) || pmd_bad(*pmd)) {
-		return;
+		return; //Check if pmd is bad or does not exist
 	}
 	
 	// Get pte from pmd and the page address
 	
-	ptep = pte_offset_map(pmd,address);
+	ptep = pte_offset_map(pmd,page_address);
 	if(!ptep) {
-		return;
+		return; //Check if pte does not exist
 	}
 		
 	pte = *ptep;
-	ptep_curr = ptep;
+	curr_pte = ptep;
 	
-	//Check if the current page is valid and whether or not it is present in the memory.
+	//Check If the page is valid and present in the memory, then it is part of the process's RSS. Increment the RSS counter variable.
 	
-	int pte_is_present = pte_present(pte);
-	
-	//If the page is valid but is not present in the memory, then it is in the SWAP. Increment the counter variable.
-	
-	if (pte_is_present == 0)
+	if ((pte_present(pte)) == 1)
 	{
-		SWAP++;
+		rss_count++;
 	}
 	
-	//If the page is valid and present in the meory, then it is in the RSS. Increment the counter variable.
+	//If the page is valid and not present in the memory, then it is in the SWAP. Increment the SWAP counter variable.
 	
 	else
 	{
-		RSS++;
+		swap_count++;
 	}
 }
 
-/* This is a timer function which helps by having a periodic timer (every 10 seconds) to help measure a given process' Resident Set Size (RSS), SWAP size, and
-Working Set Size (WSS). For several minutes , these different memory usage changes are observed and the staistics related to that are printed.*/
+/*Declaring some essential pointers to help in traversing the memory regions to help with measurments and calculations.*/
+
+struct task_struct *task; //Used to get the details of all the processes
+struct vm_area_struct *vma; //Used to traverse the memory regions.
+
+//Declarations for the 10-second periodic timer that will be defined.
 
 unsigned long timer_interval_ns = 10e9; // The 10 second interval for timers.
 static struct hrtimer timer; //Initializng the timer.
 
-//The timer function.
+//The 10 second interval timer function where the page tables are walked through and the calculations are carried out.
 
 enum hrtimer_restart no_restart_callback(struct hrtimer *timer){
 	
 	ktime_t currtime, interval;
 	currtime = ktime_get();
 	interval = ktime_set(0, timer_interval_ns);
-	hrtimer_forward(timer, currtime, interval);
+	hrtimer_forward(timer, currtime, interval); // Advances the timer's expiration time by 10 seconds from the current time.
 	
-	//Calculate the Working Set Size (WSS) for the process.
+	/*Calculate the Working Set Size (WSS) for the process. To measure the WSS, during the 10 second interval, the number of pages accessed during that period will be counted by checking the bit of every table entry.*/
 	
-	WSS = 0; //Reset the WSS for each new process.	
+	wss_count = 0; //Reset the WSS for each new process.	
 	
-	vma = task->mm->mmap;
+	vma = task->mm->mmap; //Get the memory region for the current process.
+
 	while(vma != NULL){
-		for (address = vma->vm_start; address < vma->vm_end; address += PAGE_SIZE){
+		//Loop through the memory region, checking each page.
+			
+		for (page_address = vma->vm_start; page_address < vma->vm_end; page_address += PAGE_SIZE){
 		
-			walk_page_tables(task->mm, address);
-			if (ptep_test_and_clear_young(vma, address, ptep_curr) == 1)
-				WSS++;	
+			/*Walk through each process's page tables and find out if it is present in the physical memory or in the swap.*/
+			
+			walk_page_tables(task->mm, page_address);
+			
+			/*It is checked if the current page table entry has been accessed or not using the "ptep_test_and_clear_young" function. */
+			
+			if (ptep_test_and_clear_young(vma, page_address, curr_pte) == 1)
+			/*If the given pte was proven to be accessed, the acessed bit has already been cleared, and the count of WSS can be increased. */
+			{
+				wss_count++;	
+			}
 		}
+		
+		//Move on to the next memory region.
+		
 		vma = vma->vm_next;
 	}
 	
-	// Convert all the sizes to KiloBytes.
+	// Calculate the total size of the pages in for RSS, SWAP, and WSS and convert them to KiloBytes.
 	
-	RSS = (RSS * PAGE_SIZE) / 1024;
-	SWAP = (SWAP * PAGE_SIZE) / 1024;
-	WSS = (WSS * PAGE_SIZE) / 1024;
+	unsigned rss_size = (rss_count * PAGE_SIZE) / 1024;
+	unsigned swap_size = (swap_count * PAGE_SIZE) / 1024;
+	unsigned wss_size = (wss_count * PAGE_SIZE) / 1024;
 	
-	//Print out the statistics.
+	//Print out the required statistics.
 	
-	printk(KERN_INFO "PID [%d]: RSS=%lu KB, SWAP=%lu KB, WSS=%lu KB\n", pid, RSS, SWAP, WSS);
+	printk(KERN_INFO "PID [%d]: RSS=%lu KB, SWAP=%lu KB, WSS=%lu KB\n", pid, rss_size, swap_size, wss_size);
 	
 	// Reset the counters to 0 for future processes that are going to be traversed.
 	
-	RSS = 0;
-	SWAP = 0;
+	rss_count = 0;
+	swap_count = 0;
 	
-	return HRTIMER_RESTART;
+	return HRTIMER_RESTART; //Invoke the HRT periodically.
 }
 
-//Initializing module that detects the process and starts the timer.
-//static
-int __init init_module(void){
+//The Initializing module that detects the process and starts the timer for walking through the pages.
+
+static int __init initialize_program(void){
+
+	//Pointer for traversing through the processes.
 	
-	ktime_t currtime = ktime_add(ktime_get(), ktime_set(0,10e9));
 	struct task_struct *currtask;
 	
 	for_each_process(currtask){
-	
-		if (currtask->pid == pid){
-		
+		if (currtask->pid == pid && currtask != NULL){
 			task = currtask;
+			
+			/*Initialize the timer, it is monotonic to prevent the elapsed time to ever be negative and it is in HRTIMER_ABS mode to make sure the timer is using an absolute value and not relative to the current time.*/
+			
 			hrtimer_init(&timer, CLOCK_MONOTONIC, HRTIMER_MODE_ABS);
+			
 			timer.function = &no_restart_callback;
-			hrtimer_start(&timer, currtime, HRTIMER_MODE_ABS);
+			
+			//Start the timer.
+			
+			hrtimer_start(&timer, (ktime_add(ktime_get(), ktime_set(0,10e9))), HRTIMER_MODE_ABS);
 			}
 	}
 	return 0;
@@ -158,11 +182,11 @@ int __init init_module(void){
 
 //The exiting module, cancels the timer, and exits the program.
 
-static void __exit exit_module(void){
-	hrtimer_cancel(&timer);
+static void __exit end_program(void){
+	hrtimer_cancel(&timer); //Cancels the timer.
 }
 //The initializing and exiting modules are alloted to functions.
 
-module_init(init_module);
-module_exit(exit_module);
+module_init(initialize_program);
+module_exit(end_program);
 MODULE_LICENSE("GPL");
